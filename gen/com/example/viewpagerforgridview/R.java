808512b5fa606c0002457d39460834b63ac80edd
xuqing <854264014@qq.com>
2016-10-14 13:57:47 +0800

    xuqing

--- gen/com/example/viewpagerforgridview/R.java
+++ gen/com/example/viewpagerforgridview/R.java
@@ -10,24 +10,35 @@ package com.example.viewpagerforgridview;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int gridview_gap=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int img=0x7f020001;
     }
     public static final class id {
-        public static final int gridview=0x7f060004;
-        public static final int layout=0x7f060000;
-        public static final int num1=0x7f060002;
-        public static final int num2=0x7f060003;
-        public static final int viewpager=0x7f060001;
+        public static final int gridview=0x7f070006;
+        public static final int image=0x7f070004;
+        public static final int layout=0x7f070000;
+        public static final int name=0x7f070005;
+        public static final int num1=0x7f070002;
+        public static final int num2=0x7f070003;
+        public static final int viewpager=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_cookie_list=0x7f030001;
+        public static final int fragment_cookie_list=0x7f030000;
+        public static final int goods_item=0x7f030001;
         public static final int viewpager_content=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f050000;
+        /**  设置列数 
+         */
+        public static final int gridview_column=0x7f050002;
+        /**  设置行数 
+         */
+        public static final int gridview_row=0x7f050001;
     }
     public static final class style {
         /** 
@@ -39,22 +50,11 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/example/viewpagerforgridview/CookieListFragment.java
+++ src/com/example/viewpagerforgridview/CookieListFragment.java
@@ -3,15 +3,9 @@ package com.example.viewpagerforgridview;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
@@ -24,199 +18,149 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.zifeiweb.pzz.R;
-import com.zifeiweb.pzz.activity.MenuActivity;
-import com.zifeiweb.pzz.bean.GoodsBean;
-import com.zifeiweb.pzz.commen.AppConfig;
-import com.zifeiweb.pzz.commen.Constants;
-import com.zifeiweb.pzz.commen.RequestUrls;
-import com.zifeiweb.pzz.utils.ProgressDialogUtil;
-import com.zifeiweb.pzz.utils.Util;
-import com.zifeiweb.pzz.webservice.WebService;
-
-public class CookieListFragment extends Fragment implements OnItemClickListener{
+public class CookieListFragment extends Activity implements OnItemClickListener {
 	private ViewPager viewpager;
-	private TextView num1,num2;
-	private List<View> viewList=new ArrayList<View>();
-	public static List<GoodsBean> list=new ArrayList<GoodsBean>();
+	private TextView num1, num2;
+	private List<View> viewList = new ArrayList<View>();
+	private List<Integer> list = new ArrayList<Integer>();
 	/**
 	 * 一页的商品最大个数
 	 */
-	private final static int ONE_PAGE_NUM=9;
+	private int one_page_num;
 	/**
 	 * 页数
 	 */
-	private int page_num=0;
+	private int page_num = 0;
 	/**
 	 * 最后一页的商品个数
 	 */
-	private int last_page_num=0;
-	
-	private Context context;
+	private int last_page_num = 0;
+
 	@Override
-	public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-		View view=inflater.inflate(R.layout.fragment_cookie_list, null);
-		this.context=inflater.getContext();
-		
-		init(view);
-		
-		return view;
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_cookie_list);
+
+		init();
 	}
-	
-	public void init(View view){
-		initView(view);
-		initData(getArguments().getString("type"));
+
+	public void init() {
+		initView();
+		initData();
 	}
-	
-	public void initView(View view){
-		viewpager=(ViewPager)view.findViewById(R.id.viewpager);
+
+	public void initView() {
+		viewpager = (ViewPager) findViewById(R.id.viewpager);
 		viewpager.addOnPageChangeListener(new OnPageChangeListener() {
 			@Override
 			public void onPageSelected(int arg0) {
-				num1.setText(arg0+1+"");
+				num1.setText(arg0 + 1 + "");
 			}
+
 			@Override
 			public void onPageScrolled(int arg0, float arg1, int arg2) {
 			}
+
 			@Override
 			public void onPageScrollStateChanged(int arg0) {
 			}
 		});
-		num1=(TextView)view.findViewById(R.id.num1);
-		num2=(TextView)view.findViewById(R.id.num2);
+		num1 = (TextView) findViewById(R.id.num1);
+		num2 = (TextView) findViewById(R.id.num2);
 	}
-	
-	public void initData(String type){
-		new WebService(context, new Handler(){
-			@Override
-			public void handleMessage(Message msg) {
-				Bundle _data = msg.getData();
-				switch (msg.what) {
-				case Constants.HTTP_REQUEST_START:
-					pUtil.showProgressDialog("正在加载数据...");
-					break;
-				case Constants.HTTP_REQUEST_FAILED:
-					Toast.makeText(context, "服务器返回异常，请重试", Toast.LENGTH_SHORT).show();
-					pUtil.hideProgressDialog();
-					break;
-				case Constants.HTTP_RESULT_SUCCESS:
-					pUtil.hideProgressDialog();
-					JSONObject obj1;
-					JSONArray ja1;
-					try {
-						if(_data.getString("url").endsWith(RequestUrls.GOODS_LIST)){
-							obj1 = new JSONObject(_data.getString("json").toString());
-							ja1=obj1.getJSONArray("data");
-							list.clear();
-							for(int i=0;i<ja1.length();i++){
-								GoodsBean goodsBean=new GoodsBean();
-								goodsBean.setGoods_id(ja1.getJSONObject(i).getString("goods_id"));
-								goodsBean.setGoods_name(ja1.getJSONObject(i).getString("goods_name"));
-								goodsBean.setGoods_remark(ja1.getJSONObject(i).getString("goods_remark"));
-								goodsBean.setGoods_sn(ja1.getJSONObject(i).getString("goods_sn"));
-								goodsBean.setOriginal_img(ja1.getJSONObject(i).getString("original_img"));
-								goodsBean.setShop_price(ja1.getJSONObject(i).getString("shop_price"));
-								list.add(goodsBean);
-							}
-							
-							//分页
-							page_num=(int)(list.size()/ONE_PAGE_NUM);
-							if(list.size()%ONE_PAGE_NUM!=0){
-								page_num=page_num+1;
-							}
-							//最后一页的数据条数
-							if(list.size()%ONE_PAGE_NUM!=0){
-								last_page_num=list.size()%ONE_PAGE_NUM;
-							}else{
-								last_page_num=ONE_PAGE_NUM;
-							}
-							viewList.clear();
-							//为每页填充数据
-							for(int i=0;i<page_num;i++){
-								View view;
-								if(i==page_num-1){
-									view=LayoutInflater.from(context).inflate(R.layout.viewpager_content, null);
-									GridView gridview=(GridView)view.findViewById(R.id.gridview);
-									gridview.setAdapter(new MGridViewAdapter(context,i,last_page_num));
-									gridview.setOnItemClickListener(CookieListFragment.this);
-								}else{
-									view=LayoutInflater.from(context).inflate(R.layout.viewpager_content, null);
-									GridView gridview=(GridView)view.findViewById(R.id.gridview);
-									gridview.setAdapter(new MGridViewAdapter(context,i,ONE_PAGE_NUM));
-									gridview.setOnItemClickListener(CookieListFragment.this);
-								}
-								viewList.add(view);
-							}
-							if(viewList.size()==0){
-								viewList.add(LayoutInflater.from(context).inflate(R.layout.viewpager_content, null));
-							}
-							viewpager.setAdapter(pagerAdapter);
-							
-							num1.setText("1");
-							if(page_num==0){
-								num2.setText("1");
-							}else{
-								num2.setText(page_num + "");
-							}
-						}
-					} catch (JSONException e) {
-						e.printStackTrace();
-					}
-					break;
-				case Constants.HTTP_RESULT_UNSUCCESS:
-					pUtil.hideProgressDialog();
-					JSONObject obj2;
-					try {
-						obj2 = new JSONObject(_data.get("json").toString());
-						Toast.makeText(context, obj2.getString("message"), Toast.LENGTH_SHORT).show();
-					} catch (JSONException e) {
-						e.printStackTrace();
-					}
-					break;
-				default:
-					break;
-				}
+
+	public void initData() {
+		list.clear();
+		for (int i = 0; i < 100; i++) {
+			list.add(i);
+		}
+		
+		one_page_num=Integer.parseInt(getResources().getString(R.string.gridview_row))*Integer.parseInt(getResources().getString(R.string.gridview_column));
+
+		// 分页
+		page_num = (int) (list.size() / one_page_num);
+		if (list.size() % one_page_num != 0) {
+			page_num = page_num + 1;
+		}
+		// 最后一页的数据条数
+		if (list.size() % one_page_num != 0) {
+			last_page_num = list.size() % one_page_num;
+		} else {
+			last_page_num = one_page_num;
+		}
+		viewList.clear();
+		// 为每页填充数据
+		for (int i = 0; i < page_num; i++) {
+			View view;
+			if (i == page_num - 1) {
+				view = LayoutInflater.from(this).inflate(
+						R.layout.viewpager_content, null);
+				GridView gridview = (GridView) view.findViewById(R.id.gridview);
+				gridview.setAdapter(new MGridViewAdapter(this, i, last_page_num));
+				gridview.setOnItemClickListener(CookieListFragment.this);
+			} else {
+				view = LayoutInflater.from(this).inflate(
+						R.layout.viewpager_content, null);
+				GridView gridview = (GridView) view.findViewById(R.id.gridview);
+				gridview.setAdapter(new MGridViewAdapter(this, i, one_page_num));
+				gridview.setOnItemClickListener(CookieListFragment.this);
 			}
-		}).getGoodsList(type, new AppConfig(context).getShopId());
-		//System.out.println("shopID:"+new AppConfig(context).getShopId()+";type:"+type);
+			viewList.add(view);
+		}
+		if (viewList.size() == 0) {
+			viewList.add(LayoutInflater.from(this).inflate(
+					R.layout.viewpager_content, null));
+		}
+		viewpager.setAdapter(pagerAdapter);
+
+		num1.setText("1");
+		if (page_num == 0) {
+			num2.setText("1");
+		} else {
+			num2.setText(page_num + "");
+		}
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
-		
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+
 	}
-	
-	PagerAdapter pagerAdapter = new PagerAdapter() {  
-        @Override  
-        public boolean isViewFromObject(View arg0, Object arg1) {
-            return arg0 == arg1;  
-        }  
-        @Override  
-        public int getCount() {  
-            return viewList.size();  
-        }  
-        @Override  
-        public void destroyItem(ViewGroup container, int position,Object object) {  
-            container.removeView(viewList.get(position));  
-        }  
-        @Override  
-        public Object instantiateItem(ViewGroup container, int position) { 
-            container.addView(viewList.get(position));  
-            return viewList.get(position);  
-        }  
-    };  
-    
-    class MGridViewAdapter extends BaseAdapter{
-    	Context context;
-    	int index,num;
-    	public MGridViewAdapter(Context context,int index,int num){
-    		this.context=context;
-    		this.index=index;
-    		this.num=num;
-    	}
+
+	PagerAdapter pagerAdapter = new PagerAdapter() {
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return arg0 == arg1;
+		}
+
+		@Override
+		public int getCount() {
+			return viewList.size();
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			container.removeView(viewList.get(position));
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			container.addView(viewList.get(position));
+			return viewList.get(position);
+		}
+	};
+
+	class MGridViewAdapter extends BaseAdapter {
+		Context context;
+		int index, num;
+
+		public MGridViewAdapter(Context context, int index, int num) {
+			this.context = context;
+			this.index = index;
+			this.num = num;
+		}
+
 		@Override
 		public int getCount() {
 			return num;
@@ -229,37 +173,54 @@ public class CookieListFragment extends Fragment implements OnItemClickListener{
 
 		@Override
 		public long getItemId(int position) {
-			return position+index*ONE_PAGE_NUM;
+			return position + index * one_page_num;
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			position=position+index*ONE_PAGE_NUM;
+			position = position + index * one_page_num;
 			ViewHolder viewHolder = null;
-			if(convertView==null){
-				viewHolder=new ViewHolder();
-				convertView=LayoutInflater.from(context).inflate(R.layout.goods_item, null);
-				//动态设置这个高度即可。保证该高度为上一层view的高度的三分之一，就能刚好填满全屏
-				convertView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (Util.getWindowHeight(context)-Util.dip2px(context, getResources().getDimension(R.dimen.buttom_view_up_hight)+getResources().getDimension(R.dimen.gridview_gap)))/3));
-				viewHolder.image=(ImageView)convertView.findViewById(R.id.image);
-				viewHolder.name=(TextView)convertView.findViewById(R.id.name);
-				viewHolder.price=(TextView)convertView.findViewById(R.id.price);
+			if (convertView == null) {
+				viewHolder = new ViewHolder();
+				convertView = LayoutInflater.from(context).inflate(
+						R.layout.goods_item, null);
+				// 动态设置这个高度即可。保证刚好填满viewpager界面
+				convertView.setLayoutParams(new GridView.LayoutParams(
+						ViewGroup.LayoutParams.MATCH_PARENT, (viewpager.getHeight()
+								- dip2px(context,(getResources().getDimension(R.dimen.gridview_gap)/2)*(Integer.parseInt(getResources().getString(R.string.gridview_row))-1))) 
+								/ Integer.parseInt(getResources().getString(R.string.gridview_row))));
+				
+				viewHolder.image = (ImageView) convertView
+						.findViewById(R.id.image);
+				viewHolder.name = (TextView) convertView
+						.findViewById(R.id.name);
 				convertView.setTag(viewHolder);
-			}else{
-				viewHolder=(ViewHolder)convertView.getTag();
+			} else {
+				viewHolder = (ViewHolder) convertView.getTag();
+				
+				//这里也必须添加这个，否则第一个item会显示不正常
+				convertView.setLayoutParams(new GridView.LayoutParams(
+						ViewGroup.LayoutParams.MATCH_PARENT, (viewpager.getHeight()
+								- dip2px(context,(getResources().getDimension(R.dimen.gridview_gap)/2)*(Integer.parseInt(getResources().getString(R.string.gridview_row))-1))) 
+								/ Integer.parseInt(getResources().getString(R.string.gridview_row))));
 			}
-			
-			ImageLoader.getInstance().displayImage(AppConfig.URL_HTTP_IMG+list.get(position).getOriginal_img(), viewHolder.image);
-			viewHolder.name.setText(list.get(position).getGoods_name());
-			viewHolder.price.setText("￥"+list.get(position).getShop_price()+"/份");
-			
+
+			viewHolder.image.setImageResource(R.drawable.img);
+			viewHolder.name.setText("第"+(position+1)+"个");
 			return convertView;
 		}
-		
-		class ViewHolder{
+
+		class ViewHolder {
 			ImageView image;
 			TextView name;
-			TextView price;
 		}
-    }
+	}
+	
+	/** 
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素) 
+     */  
+    public static int dip2px(Context context, float dpValue) {  
+        final float scale = context.getResources().getDisplayMetrics().density;  
+        return (int) (dpValue * scale + 0.5f);  
+    }  
 }
